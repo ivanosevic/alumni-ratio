2a838fee98d7f7066d452dbf3f19b9d175b24806
armandonoel2022 <armandonoel@outlook.com>
2023-02-28 19:55:11 -0400

    Solving a typo in 'GeneralJourneyRoy', it should be 'GeneralJournalRow'.

--- alumni-ratio-backend/src/main/java/edu/pucmm/eict/GeneralJournal.java
+++ alumni-ratio-backend/src/main/java/edu/pucmm/eict/GeneralJournal.java
@@ -1,17 +1,31 @@
 package edu.pucmm.eict;
 
+import javax.swing.*;
+import java.util.List;
+
 //Este es el diario general en el que se registran las primeras transacciones
 public class GeneralJournal
 {
-    private Transaction GeneralJournalEntry[];
-
-    public GeneralJournal(Transaction[] generalJournalEntry)
+    private Transaction transaction;
+    private List<Transaction> list;
     {
-        GeneralJournalEntry = generalJournalEntry;
+        GeneralJournalEntry generalJournalEntry;
+        generalJournalEntry = new GeneralJournalEntry(transaction);
     }
 
-    public Transaction[] getGeneralJourneyEntry()
+    public GeneralJournal(Transaction transaction, List<Transaction> list)
     {
-        return GeneralJournalEntry;
+        this.transaction = transaction;
+        this.list = list;
+    }
+
+    public Transaction getTransaction()
+    {
+        return transaction;
+    }
+
+    public List<Transaction> getList()
+    {
+        return list;
     }
 }
