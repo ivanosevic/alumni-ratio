aa0414e5cd249864d28037fc6a5555ebb763627d
armandonoel2022 <armandonoel@outlook.com>
2023-03-06 22:17:24 -0400

    Adding Javalin to the project

--- alumni-ratio-backend/src/main/java/edu/pucmm/eict/AlumniRatioApplication.java
+++ alumni-ratio-backend/src/main/java/edu/pucmm/eict/AlumniRatioApplication.java
@@ -1,8 +1,14 @@
 package edu.pucmm.eict;
 
-public class AlumniRatioApplication
-{
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+import io.javalin.Javalin;
+
+public class AlumniRatioApplication {
+    public static void main(String[] args)
+    {
+        var app = Javalin.create(/*config*/)
+                .get("/", ctx -> ctx.result("Hello World"))
+                .start(7070);
+
     }
 }
