9863303699318273db76bee5136392a103e17436
ivanoseviic <garcia@ivanosevic.com>
2023-03-09 18:07:55 -0800

    add documentation with openapi

--- alumni-ratio-backend/src/main/java/edu/pucmm/eict/AlumniRatioApplication.java
+++ alumni-ratio-backend/src/main/java/edu/pucmm/eict/AlumniRatioApplication.java
@@ -1,11 +1,34 @@
 package edu.pucmm.eict;
 
 import io.javalin.Javalin;
+import io.javalin.openapi.plugin.OpenApiConfiguration;
+import io.javalin.openapi.plugin.OpenApiPlugin;
+import io.javalin.openapi.plugin.OpenApiPluginConfiguration;
+import io.javalin.openapi.plugin.swagger.SwaggerConfiguration;
+import io.javalin.openapi.plugin.swagger.SwaggerPlugin;
 
 public class AlumniRatioApplication {
-    public static void main(String[] args)
-    {
-        var app = Javalin.create();
+    public static void main(String[] args) {
+        var app = Javalin.create(configuration -> {
+            configuration.plugins.register(new OpenApiPlugin(
+                    new OpenApiPluginConfiguration()
+                            .withDocumentationPath("/openapi")
+                            .withDefinitionConfiguration((version, definition) -> definition
+                                    .withOpenApiInfo((openApiInfo) -> {
+                                        openApiInfo.setTitle("Alumni Ratio API");
+                                        openApiInfo.setVersion("1.0.0");
+                                    })
+                                    .withServer((openApiServer) -> {
+                                        openApiServer.setUrl(("http://localhost:{port}/" + version + "/"));
+                                        openApiServer.setDescription("Server description goes here");
+                                        openApiServer.addVariable("port", "7000", new String[]{"7070", "8080"}, "Port of the server");
+                                    })
+                            )
+            ));
+
+            SwaggerConfiguration swaggerConfiguration = new SwaggerConfiguration();
+            configuration.plugins.register(new SwaggerPlugin(swaggerConfiguration));
+        });
         var exerciseController = new ExerciseController();
         app.post("/solve-exercise", exerciseController::solveExercise);
         app.start(7000);
--- alumni-ratio-backend/src/main/java/edu/pucmm/eict/ExerciseController.java
+++ alumni-ratio-backend/src/main/java/edu/pucmm/eict/ExerciseController.java
@@ -1,6 +1,12 @@
 package edu.pucmm.eict;
 
+import io.javalin.http.BadRequestResponse;
 import io.javalin.http.Context;
+import io.javalin.http.InternalServerErrorResponse;
+import io.javalin.openapi.HttpMethod;
+import io.javalin.openapi.OpenApi;
+import io.javalin.openapi.OpenApiContent;
+import io.javalin.openapi.OpenApiResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,6 +14,18 @@ public class ExerciseController {
 
     private final static Logger logger = LoggerFactory.getLogger(ExerciseController.class);
 
+    @OpenApi(
+            summary = "Solves a simple accounting exercise",
+            operationId = "solveExercise",
+            path = "/solve-exercise",
+            methods = HttpMethod.POST,
+            tags = {"Exercises"},
+            responses = {
+                    @OpenApiResponse(status = "200", content = {@OpenApiContent(from = SolvedExercise.class)}),
+                    @OpenApiResponse(status = "400", content = {@OpenApiContent(from = BadRequestResponse.class)}),
+                    @OpenApiResponse(status = "500", content = {@OpenApiContent(from = InternalServerErrorResponse.class)})
+            }
+    )
     public void solveExercise(Context ctx) {
         Exercise exercise = ctx.bodyAsClass(Exercise.class);
         logger.info("{}", exercise);
