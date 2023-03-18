aa791356580b6aef58b140672032be209a3c3d06
ivanoseviic <garcia@ivanosevic.com>
2023-03-18 14:17:31 -0700

    add general information form to exercise form

--- frontend/src/exercises/AccountingExerciseForm.tsx
+++ frontend/src/exercises/AccountingExerciseForm.tsx
@@ -1,14 +1,35 @@
-import {useState} from "react";
+import React, {useState} from "react";
 import ExerciseForm, {ExerciseFormModel} from "./ExerciseForm";
+import {Steps, StepsSelectEvent} from "primereact/steps";
+import {MenuItem} from "primereact/menuitem";
 import {Card} from "primereact/card";
 
 function AccountingExerciseForm() {
   const [exerciseFormModel, setExerciseFormModel] = useState<ExerciseFormModel>(new ExerciseFormModel());
+  const [activeIndex, setActiveIndex] = useState(0);
+  const items: MenuItem[] = [
+    {
+      label: 'Informaciones Generales'
+    },
+    {
+      label: 'Transacciones'
+    },
+    {
+      label: 'Confirmación',
+    }
+  ];
 
   return (
       <>
-        <Card title={'Exercise Form'}>
-          <ExerciseForm exerciseForm={exerciseFormModel} setExerciseFormModel={setExerciseFormModel} />
+        <Card>
+          <Steps className="mb-5" model={items} activeIndex={activeIndex}
+                 onSelect={(e: StepsSelectEvent) => setActiveIndex(e.index)}
+                 readOnly={true}/>
+
+          <section className="active-section">
+            {activeIndex === 0 ?
+                <ExerciseForm exerciseForm={exerciseFormModel} setExerciseFormModel={setExerciseFormModel}/> : null}
+          </section>
         </Card>
       </>
   )
