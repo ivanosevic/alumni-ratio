224c24e2dca0fc092d4b609c675692fae7bbda4a
ivanoseviic <garcia@ivanosevic.com>
2023-03-18 11:23:56 -0700

    separate main plugins configurations to different class

--- alumni-ratio-backend/src/main/java/edu/pucmm/eict/AlumniRatioApplication.java
+++ alumni-ratio-backend/src/main/java/edu/pucmm/eict/AlumniRatioApplication.java
@@ -1,33 +1,17 @@
 package edu.pucmm.eict;
 
+import edu.pucmm.eict.configuration.PluginsConfiguration;
 import edu.pucmm.eict.exercises.ExerciseController;
 import edu.pucmm.eict.reports.exceptions.PDFGenerationErrorException;
 import io.javalin.Javalin;
-import io.javalin.openapi.plugin.OpenApiPlugin;
-import io.javalin.openapi.plugin.OpenApiPluginConfiguration;
-import io.javalin.openapi.plugin.swagger.SwaggerConfiguration;
-import io.javalin.openapi.plugin.swagger.SwaggerPlugin;
+
 
 public class AlumniRatioApplication {
     public static void main(String[] args) {
         var app = Javalin.create(configuration -> {
-            configuration.plugins.register(new OpenApiPlugin(
-                    new OpenApiPluginConfiguration()
-                            .withDocumentationPath("/openapi")
-                            .withDefinitionConfiguration((version, definition) -> definition
-                                    .withOpenApiInfo((openApiInfo) -> {
-                                        openApiInfo.setTitle("Alumni Ratio API");
-                                        openApiInfo.setVersion("1.0.0");
-                                    })
-                                    .withServer((openApiServer) -> {
-                                        openApiServer.setUrl(("http://localhost:7000/"));
-                                        openApiServer.setDescription("Server description goes here");
-                                    })
-                            )
-            ));
-
-            SwaggerConfiguration swaggerConfiguration = new SwaggerConfiguration();
-            configuration.plugins.register(new SwaggerPlugin(swaggerConfiguration));
+            configuration.plugins.register(PluginsConfiguration.openApiPluginConfiguration());
+            configuration.plugins.register(PluginsConfiguration.swaggerConfiguration());
+            configuration.jsonMapper(PluginsConfiguration.jsonMapper());
         });
         var exerciseController = new ExerciseController();
         app.post("/solve-exercise", exerciseController::solveExercise);
