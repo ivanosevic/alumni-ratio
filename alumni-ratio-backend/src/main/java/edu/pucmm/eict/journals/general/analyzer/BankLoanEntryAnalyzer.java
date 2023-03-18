a919d375c7f4ca8d37ced3575c71aad5462582a7
ivanoseviic <garcia@ivanosevic.com>
2023-03-18 09:29:29 -0700

    add logic for bank loan and bank payment entries

--- alumni-ratio-backend/src/main/java/edu/pucmm/eict/journals/general/analyzer/BankLoanEntryAnalyzer.java
+++ alumni-ratio-backend/src/main/java/edu/pucmm/eict/journals/general/analyzer/BankLoanEntryAnalyzer.java
@@ -1,12 +1,24 @@
 package edu.pucmm.eict.journals.general.analyzer;
 
+import edu.pucmm.eict.accounts.AccountBook;
 import edu.pucmm.eict.exercises.Exercise;
 import edu.pucmm.eict.journals.general.GeneralJournalEntry;
+import edu.pucmm.eict.journals.general.GeneralJournalRow;
 import edu.pucmm.eict.transactions.Transaction;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 public class BankLoanEntryAnalyzer implements GeneralJournalEntryAnalyzer {
     @Override
     public GeneralJournalEntry analyzeTransaction(Transaction transaction, Exercise exercise) {
-        return null;
+        var entryExplanation = "Prestamo bancario";
+        var transactionDate = LocalDate.of(exercise.getYearOperations(), exercise.getMonthOperations(), transaction.getDay());
+        var entry = new GeneralJournalEntry(transactionDate, entryExplanation);
+        var cashAccountName = "Efectivo";
+        var notesPayableAccountName = "Documentos por pagar";
+        entry.addRow(new GeneralJournalRow(transactionDate, AccountBook.CASH, cashAccountName, transaction.getAmount(), BigDecimal.ZERO));
+        entry.addRow(new GeneralJournalRow(transactionDate, AccountBook.NOTES_PAYABLES, notesPayableAccountName, BigDecimal.ZERO, transaction.getAmount()));
+        return entry;
     }
 }
--- alumni-ratio-backend/src/main/java/edu/pucmm/eict/journals/general/analyzer/BankLoanPaymentEntryAnalyzer.java
+++ alumni-ratio-backend/src/main/java/edu/pucmm/eict/journals/general/analyzer/BankLoanPaymentEntryAnalyzer.java
@@ -1,12 +1,24 @@
 package edu.pucmm.eict.journals.general.analyzer;
 
+import edu.pucmm.eict.accounts.AccountBook;
 import edu.pucmm.eict.exercises.Exercise;
 import edu.pucmm.eict.journals.general.GeneralJournalEntry;
+import edu.pucmm.eict.journals.general.GeneralJournalRow;
 import edu.pucmm.eict.transactions.Transaction;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 public class BankLoanPaymentEntryAnalyzer implements GeneralJournalEntryAnalyzer {
     @Override
     public GeneralJournalEntry analyzeTransaction(Transaction transaction, Exercise exercise) {
-        return null;
+        var entryExplanation = String.format("Pago de prestamo bancario del día %s", transaction.getTransactionReference());
+        var transactionDate = LocalDate.of(exercise.getYearOperations(), exercise.getMonthOperations(), transaction.getDay());
+        var entry = new GeneralJournalEntry(transactionDate, entryExplanation);
+        var cashAccountName = "Efectivo";
+        var notesPayableAccountName = "Documentos por pagar";
+        entry.addRow(new GeneralJournalRow(transactionDate, AccountBook.NOTES_PAYABLES, notesPayableAccountName, transaction.getAmount(), BigDecimal.ZERO));
+        entry.addRow(new GeneralJournalRow(transactionDate, AccountBook.CASH, cashAccountName, BigDecimal.ZERO, transaction.getAmount()));
+        return entry;
     }
 }
