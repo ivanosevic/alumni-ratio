ed6f973b24cf586339e5840d626405cac1a0f417
ivanoseviic <garcia@ivanosevic.com>
2023-03-20 17:51:44 -0700

    add basics for general journal table pdf generation

--- alumni-ratio-backend/src/main/java/edu/pucmm/eict/reports/SolvedExerciseReportService.java
+++ alumni-ratio-backend/src/main/java/edu/pucmm/eict/reports/SolvedExerciseReportService.java
@@ -2,6 +2,7 @@ package edu.pucmm.eict.reports;
 
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
 import edu.pucmm.eict.exercises.SolvedExercise;
 import edu.pucmm.eict.reports.models.SolvedExerciseReport;
 import edu.pucmm.eict.reports.sections.*;
@@ -15,18 +16,21 @@ public class SolvedExerciseReportService {
         var pdfByteArray = new ByteArrayOutputStream();
         var pdfWriter = new PdfWriter(pdfByteArray);
         PdfDocument report = new PdfDocument(pdfWriter);
+        var document = new Document(report);
 
-        var generalInformationSection = new GeneralInformationSection(report, solvedExercise);
-        var transactionSection = new TransactionSection(report, solvedExercise);
-        var generalJournalSection = new GeneralJournalSection(report, solvedExercise);
-        var generalLedgerSection = new GeneralLedgerSection(report, solvedExercise);
-        var trialBalanceSection = new TrialBalanceSection(report, solvedExercise);
+        var generalInformationSection = new GeneralInformationSection(report, document, solvedExercise);
+        var transactionSection = new TransactionSection(report, solvedExercise, document);
+        var generalJournalSection = new GeneralJournalSection(report, solvedExercise, document);
+        var generalLedgerSection = new GeneralLedgerSection(report, solvedExercise, document);
+        var trialBalanceSection = new TrialBalanceSection(report, solvedExercise, document);
 
         generalInformationSection.generateSection();
         transactionSection.generateSection();
         generalJournalSection.generateSection();
         generalLedgerSection.generateSection();
         trialBalanceSection.generateSection();
+
+        document.close();
 
         return new SolvedExerciseReport(pdfByteArray.toByteArray(), LocalDateTime.now());
     }
--- alumni-ratio-backend/src/main/java/edu/pucmm/eict/reports/sections/GeneralLedgerSection.java
+++ alumni-ratio-backend/src/main/java/edu/pucmm/eict/reports/sections/GeneralLedgerSection.java
@@ -1,12 +1,13 @@
 package edu.pucmm.eict.reports.sections;
 
 import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.layout.Document;
 import edu.pucmm.eict.exercises.SolvedExercise;
 
 public class GeneralLedgerSection extends SolvedExercisePDFSection {
 
-    public GeneralLedgerSection(PdfDocument pdfDocument, SolvedExercise solvedExercise) {
-        super(pdfDocument, solvedExercise);
+    public GeneralLedgerSection(PdfDocument pdfDocument, SolvedExercise solvedExercise, Document document) {
+        super(pdfDocument, solvedExercise, document);
     }
 
     @Override
